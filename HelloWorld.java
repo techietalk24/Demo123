e636ba8ad7de72430e7fd83bb3e4240f4e30a391
techietalk24 <techietalk24@gmail.com>
2025-11-04 12:52:29 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+
+public static void main(String args[]){
+System.out.println("Hello Working with Core Java Programming!!");
+System.out.println("Test the code....");
+}
+}
